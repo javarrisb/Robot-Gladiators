1fc1194c7d0889476009c925cc3e7142d9afa9b5
Javarris Barnett <javarrisb@gmail.con>
2021-10-17 02:27:30 +0200

    Added JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this 
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
