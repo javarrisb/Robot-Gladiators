3e7318093ed48ffbabf2d1ccbb26dbd1eadc289f
Javarris Barnett <javarrisb@gmail.con>
2021-10-17 02:41:05 +0200

    Set instructions for code

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,7 +10,16 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function() {
+    // Alert the players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
+
+    // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to updat the value in the `enemyHealth` variable
+
+    // Log a resulting message to the console so we know that it worked.
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+
+    // Log a resulting message to the console so we know that it worked.
 };
 
 fight();
