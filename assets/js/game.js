9c26115071fa22fcf2708fd5c0ca160894e5014c
Javarris Barnett <javarrisb@gmail.con>
2021-10-17 23:12:11 +0200

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,8 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var playerMoney = 10
+
 var fight = function() {
     // Alert the players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
@@ -42,10 +44,23 @@ var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? En
     } 
     // if player chooses to skip
     }  else if (promptFight === "skip" || promptFight === "SKIP") {
-        window.alert(playerName + " has chosen to skip the fight!");
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        //if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            // suntract money from playerMoney for skipping
+            playerMoney = playerMoney - 2;
+        }
+        // if no (false), ask question again by running fight() again
+        else {
+            fight();
+        }
     } else {
         window.alert("You need to choose a valid option. Try again!");
-    }
+        }
+
     }
 
 fight();
