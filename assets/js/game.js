d1287be83fd8528e60c96fdb8deaba1146c0c948
Javarris Barnett <javarrisb@gmail.con>
2021-10-17 03:03:16 +0200

    Added actions to the fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,12 +14,16 @@ var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
 
     // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to updat the value in the `enemyHealth` variable
-
+    enemyHealth = enemyHealth - playerAttack;
     // Log a resulting message to the console so we know that it worked.
-
+    playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining. "
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-
+    playerHealth = playerHealth - enemyAttack
     // Log a resulting message to the console so we know that it worked.
-};
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. " 
+        
+    );
+    };
 
 fight();
