c0612f8392c2fc679d63e47c164dc634b9036a92
Javarris Barnett <javarrisb@gmail.con>
2021-10-19 23:34:48 +0200

    Added endgame() function

--- assets/js/game.js
+++ assets/js/game.js
@@ -90,7 +90,7 @@ var startGame = function () {
       enemyHealth = 50;
 
       // use debugger to pause script from running and check what's going on at that moment in the code
-       debugger;
+      // debugger;
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
@@ -100,8 +100,31 @@ var startGame = function () {
       break;
     }
   }
-  // play again
-  startGame();
+  // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+  endGame();
 };
+
+// function to end the entire game
+var endGame = function () {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of  " + playerMoney + ".");
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+  // ask player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    //restart the game
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+};
+
 
 startGame();
